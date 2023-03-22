8ab54ed05b570d94d158e05d24cd8aac25e45b87
Marco von Baumbach <marco.vonbaumbach@gmx.de>
2023-03-22 14:25:47 +0100

    portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -9,44 +9,16 @@ export class PortfolioComponent {
 
   constructor() {}
 
-  openJoin() {
+  openProject(param: string) {
     window.open(
-      'https://www.marco-von-baumbach.developerakademie.net/join',
+      `https://www.marco-von-baumbach.developerakademie.net/${param}`,
       '_blank'
     );
   }
 
-  openElPolloLoco() {
+  openGithub(param: string) {
     window.open(
-      'https://www.marco-von-baumbach.developerakademie.net/elPolloLoco',
-      '_blank'
-    );
-  }
-
-  openRingOfFire() {
-    window.open(
-      'https://marco-von-baumbach.developerakademie.net/ringoffire',
-      '_blank'
-    );
-  }
-
-  githubRingOfFire() {
-    window.open(
-      'https://github.com/MarcovonBaumbach/ringoffire',
-      '_blank'
-    );
-  }
-
-  githubJoin() {
-    window.open(
-      'https://github.com/MarcovonBaumbach/Join',
-      '_blank'
-    );
-  }
-
-  githubElPolloLoco() {
-    window.open(
-      'https://github.com/MarcovonBaumbach/ElPolloLoco',
+      `https://github.com/MarcovonBaumbach/${param}`,
       '_blank'
     );
   }
